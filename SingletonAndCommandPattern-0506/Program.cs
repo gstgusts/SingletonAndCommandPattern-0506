acac267ef1cb1a86a9f5594021a6bd47c998240c
Gusts Linkevics <gusts@gst.lv>
2023-07-18 20:42:40 +0300

    Add project files.

---
+++ SingletonAndCommandPattern-0506/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SingletonAndCommandPattern_0506
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
